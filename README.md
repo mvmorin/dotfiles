b1be5eed4edd57b2ce321ed09d98d8df3c5712a7
Martin Morin Vikström <martinm@zakai.control.lth.se>
2018-06-11 17:08:54 +0200

    Add simple install script to symlink dotfiles

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 config-, rc- and dotfiles
 
 nothing fancy yet, just manual backup and sync
+"create-symlinks.sh" does just that, given that the dotfiles are in the list
 
+# ToDo
 This might be a good approach
 https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
---
+++ create-symlinks.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+############################
+# This script creates symlinks from the home directory to any desired dotfiles
+# in ~/dotfiles
+############################
+
+########## Variables
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files=".bashrc .bash_profile .vim .i3"    # list of files/folders to symlink
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles."
+mkdir -p $olddir
+
+# change to the dotfiles directory
+cd $dir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create
+# symlinks 
+echo -e "\nCreating symlink to dotfiles, backing up any already existing files."
+for file in $files; do
+	mv ~/$file ~/dotfiles_old/
+	echo -e "\tCreating symlink to $file in home directory."
+	ln -s $dir/$file ~/$file
+done
+
+echo -e "\nDone, delete $oldir if everything is ok."
